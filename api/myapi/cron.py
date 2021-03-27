56edfad5d1c6db00d5c920046ddf9f8bada4a5cc
Marek Prochazka <m.prochazka2002@gmail.com>
2021-03-27 10:58:11 +0100

    cron test

--- api/myapi/cron.py
+++ api/myapi/cron.py
@@ -1,6 +1,8 @@
 from .models import DailyId
 import random
 
+
 def generate_new_daily_id():
     DailyId.objects.all().delete()
     DailyId(random.randint(1, 48)).save()
+
---
+++ api/myapi/generate_new_daily_id.py
@@ -0,0 +1,3 @@
+from .cron import generate_new_daily_id
+
+generate_new_daily_id()
