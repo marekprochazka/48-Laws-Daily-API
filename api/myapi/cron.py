d71f456b3d146ff49251a2256014773813da2eca
Marek Prochazka <m.prochazka2002@gmail.com>
2021-03-30 19:50:51 +0200

    id generation limit

--- api/myapi/cron.py
+++ api/myapi/cron.py
@@ -4,5 +4,6 @@ import random
 
 def generate_new_daily_id():
     DailyId.objects.all().delete()
-    DailyId(random.randint(1, 48)).save()
+    # DailyId(random.randint(1, 48)).save() Limited to actual laws in db
+    DailyId(random.randint(1, 36)).save()
 
